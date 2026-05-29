c758a911669f5e70568e60519e16f5620cae5bed
Erwin Asseng <erwinalberic9@gmail.com>
2026-05-29 13:55:15 +0100

    ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
+        loadComponent: () => import('./page/home/home.component').then(m => m.HomeComponent)   
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./page/login/login.component').then(m => m.LoginComponent) 
     },
     {
         path: '**',
