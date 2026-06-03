bc907981719c0beb36377cdd5679526021ec2e06
Erwin Asseng <erwinalberic9@gmail.com>
2026-06-03 10:48:51 +0100

    ok 3

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { environment } from '../../environnement/environment'; // Assurez-vous d'avoir ce fichier
+import { environment } from '../../environnement/environment.prod'; // Assurez-vous d'avoir ce fichier
 
 @Injectable({
   providedIn: 'root'
